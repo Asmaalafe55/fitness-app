befaa0d1b9e8976787f3aa3eaf48f9ce96a82af2
Mario Saliba <mario.saliba98@gmail.com>
2023-04-12 17:02:25 +0300

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['page.jsx'],
+
 }
 
 module.exports = nextConfig
---
+++ pages/login/Login.jsx
@@ -0,0 +1,8 @@
+const Login = () => {
+    return (
+        <h1>Hello</h1>
+    )
+
+}
+
+export default Login
