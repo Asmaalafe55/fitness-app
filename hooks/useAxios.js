9dcbd02d2d5ca5146891cc867b8ffd6beab82ae9
Saher Hanif <saher.ha97@gmail.com>
2023-04-12 23:34:16 +0300

    fixed eslint issue

--- hooks/useAxios.js
+++ hooks/useAxios.js
@@ -2,28 +2,28 @@ import { useState, useEffect } from 'react'
 import axios from '@/lib/axios'
 
 const useAxios = ({ url, method, body = null, headers = null }) => {
-    const [response, setResponse] = useState(null)
-    const [error, setError] = useState('')
-    const [loading, setloading] = useState(true)
+  const [response, setResponse] = useState(null)
+  const [error, setError] = useState('')
+  const [loading, setloading] = useState(true)
 
-    const fetchData = () => {
-        axios[method](url, JSON.parse(headers), JSON.parse(body))
-            .then((res) => {
-                setResponse(res.data)
-            })
-            .catch((err) => {
-                setError(err)
-            })
-            .finally(() => {
-                setloading(false)
-            })
-    }
+  const fetchData = () => {
+    axios[method](url, JSON.parse(headers), JSON.parse(body))
+      .then((res) => {
+        setResponse(res.data)
+      })
+      .catch((err) => {
+        setError(err)
+      })
+      .finally(() => {
+        setloading(false)
+      })
+  }
 
-    useEffect(() => {
-        fetchData()
-    }, [method, url, body, headers])
+  useEffect(() => {
+    fetchData()
+  }, [method, url, body, headers])
 
-    return { response, error, loading }
+  return { response, error, loading }
 }
 
 export default useAxios
