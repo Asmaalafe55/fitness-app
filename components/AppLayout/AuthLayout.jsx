3a26008837d456b25c8498dc11d8a9046b93f448
Asmaalafe55 <asmaalafe55@gmail.com>
2023-04-14 22:21:58 +0300

    adding images + editing the layout

--- components/AppLayout/AuthLayout.jsx
+++ components/AppLayout/AuthLayout.jsx
@@ -1,14 +1,9 @@
-
 // import style from './authLayout.module.scss'
 import { useRecoilState } from 'recoil'
 
-
 const AuthLayout = ({ children }) => {
-
   // Customize the auth layout here
-  return (
-    <div style={{ padding: '24px 12px', minHeight: 360 }}>{children}</div>
-  )
+  return <div style={{ padding: '24px 12px', minHeight: 360 }}>{children}</div>
 }
 
 export default AuthLayout
--- public/images/images.js
+++ public/images/images.js
@@ -1,5 +1,10 @@
 import pic1 from '../images/landing_page.png'
+import pic2 from '../images/logo.png'
+import pic3 from '../images/menu-01.png'
+import pic4 from '../images/Moon.png'
+import pic5 from '../images/Remove.png'
+import pic6 from '../images/Sun.png'
 
-const images = [pic1]
+const images = [pic1, pic2, pic3, pic4, pic5, pic6]
 
 export default images
