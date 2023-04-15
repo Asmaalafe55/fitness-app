8004b653d6e8841498c628d92c4e9cf40ea51c09
Asmaalafe55 <asmaalafe55@gmail.com>
2023-04-16 00:43:31 +0300

    layout

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ['page.jsx'],
-
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
