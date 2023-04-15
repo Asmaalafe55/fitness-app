4ef73d9ecacb71df286d151c959b1fed769f19c3
Asmaalafe55 <asmaalafe55@gmail.com>
2023-04-16 01:48:24 +0300

    removing the icons

--- public/images/images.js
+++ public/images/images.js
@@ -1,14 +1,9 @@
 import pic1 from '../images/landing_page.png'
 
-import i_menu from '../images/menu-01.png'
-import i_remove from '../images/Remove.png'
-import i_moon from '../images/Moon.png'
-import i_sun from '../images/Sun.png'
-
 import logo from '../images/logo.png'
 import logo_fit from '../images/logo-fitnesspark.png'
 
-const images = [pic1, i_menu, i_remove, i_moon, i_sun]
+const images = [pic1]
 export const imagesArray = [logo, logo_fit]
 
 export default images
