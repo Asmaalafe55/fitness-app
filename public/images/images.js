db892736d0c12715059ba5fb75e377da78119986
Asmaalafe55 <asmaalafe55@gmail.com>
2023-04-15 16:56:27 +0300

    adding logo pic

--- public/images/images.js
+++ public/images/images.js
@@ -4,7 +4,8 @@ import pic3 from '../images/menu-01.png'
 import pic4 from '../images/Moon.png'
 import pic5 from '../images/Remove.png'
 import pic6 from '../images/Sun.png'
+import pic7 from '../images/logo-fitnesspark.png'
 
-const images = [pic1, pic2, pic3, pic4, pic5, pic6]
+const images = [pic1, pic2, pic3, pic4, pic5, pic6, pic7]
 
 export default images
