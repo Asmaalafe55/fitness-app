877d87ae33f82d1b62274a969514fdab6e96b67d
Asmaalafe55 <asmaalafe55@gmail.com>
2023-04-21 23:57:36 +0300

    adding fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import AppLayout from '@/components/AppLayout/AppLayout'
 import '@/styles/globals.css'
+import '../styles/fonts.module.scss'
 import { RecoilRoot } from 'recoil'
 
 export default function App({ Component, pageProps }) {
