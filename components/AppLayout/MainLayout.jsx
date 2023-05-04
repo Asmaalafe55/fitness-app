78bcc5b7eaabdee6a24747898ceb5550302cde56
Asmaalafe55 <asmaalafe55@gmail.com>
2023-05-04 21:37:21 +0300

    editing the style

--- components/AppLayout/MainLayout.jsx
+++ components/AppLayout/MainLayout.jsx
@@ -1,10 +1,15 @@
 import { useRouter } from 'next/router'
+import style from './MainLayout.module.scss'
 
 const MainLayout = ({ children }) => {
   const router = useRouter()
 
   return (
     <>
+      <div className={style.main_sidebar}></div>
+      <div className={style.main_navbar}>
+        <div className={style.logo_style}></div>
+      </div>
       <div>{children}</div>
     </>
   )
