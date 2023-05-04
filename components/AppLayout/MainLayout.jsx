19789322f8e0d370ffdbdd0033350cf80e617dcd
Asmaalafe55 <asmaalafe55@gmail.com>
2023-05-04 23:15:28 +0300

    editing the style

--- components/AppLayout/MainLayout.jsx
+++ components/AppLayout/MainLayout.jsx
@@ -4,13 +4,28 @@ import Link from 'next/link'
 import style from './MainLayout.module.scss'
 import imagesArray from '../../public/images/images'
 
+import { BiHeart, BiUser, BiMessageRounded } from 'react-icons/bi'
+import {
+  MdOutlineNotificationsNone,
+  MdOutlineTaskAlt,
+  MdOutlineAccountCircle
+} from 'react-icons/md'
+
 const MainLayout = ({ children }) => {
   const router = useRouter()
 
   return (
     <>
       <div className={style.main_sidebar}>
-        <Image src={imagesArray.logo} alt="logo" width={45} />
+        <Link href="/">
+          <Image src={imagesArray.logo} alt="logo" width={45} />
+        </Link>
+        <div className={style.icons_section}>
+          <MdOutlineTaskAlt />
+          <BiMessageRounded />
+          <MdOutlineAccountCircle />
+          <BiHeart />
+        </div>
       </div>
       <div className={style.main_navbar}></div>
       <div>{children}</div>
