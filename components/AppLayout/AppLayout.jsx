e18871dd5f57451516931f214cee9e75e72d6e1b
Asmaalafe55 <asmaalafe55@gmail.com>
2023-06-01 20:18:32 +0300

    box shadow

--- components/AppLayout/AppLayout.jsx
+++ components/AppLayout/AppLayout.jsx
@@ -10,38 +10,6 @@ const AppLayout = ({ children }) => {
   const [userData, setUserData] = useRecoilState(userDataAtom)
   const router = useRouter()
 
-  //   const user = getUserData(); // Retrieve the user data from your storage or state
-
-  //   useEffect(() => {
-  //     if (router.pathname.includes('/auth/')) {
-  //       return;
-  //     }
-
-  //     if (!user?.data) {
-  //       if (!router.pathname.includes('auth')) {
-  //         localStorage.setItem('redirect', router.pathname);
-  //         router.push('/auth/login');
-  //       }
-  //     }
-  //   }, [router, user]);
-
-  //   if (router.pathname.includes('/dashboard/')) {
-  //     if (!user?.data) {
-  //       // Redirect the user to the login page if not authenticated
-  //       router.push('/auth/login');
-  //       return null; // Return null while the redirection happens
-  //     }
-
-  //     return <AuthLayout>{children}</AuthLayout>;
-  //   }
-
-  //   if (router.pathname === '/404') {
-  //     return <AuthLayout>{children}</AuthLayout>;
-  //   }
-
-  //   return <MainLayout>{children}</MainLayout>;
-  // }
-
   if (router.pathname.includes('/dashboard/') || router.pathname === '/404') {
     return <MainLayout>{children}</MainLayout>
   }
@@ -50,22 +18,3 @@ const AppLayout = ({ children }) => {
 }
 
 export default AppLayout
-
-// useEffect(() => {
-//   // if (router.pathname.includes('/auth/')) {
-//   //   return () => {}
-//   // }
-//   // if (user?.data) {
-//   //   setUserData({ role: user.data.role })
-//   // }
-//   // if (!isLoading) {
-//   //   if (!user?.data) {
-//   //     // we don't want the user to redirect back to the login page
-//   //     // or any auth page
-//   //     if (!router.pathname.includes('auth')) {
-//   //       localStorage.setItem('redirect', router.pathname)
-//   //       // router.push('/auth/login')
-//   //     }
-//   //   }
-//   // }
-// }, [router, setUserData])
